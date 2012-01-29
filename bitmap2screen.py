f87b9dac33175f79ccbf282e3104d9326a37077a
guyzmo <guyzmo+github@m0g.net>
2012-01-29 16:53:36 +0100

    updated matrix adapt

--- bitmap2screen.py
+++ bitmap2screen.py
@@ -1,51 +1,63 @@
 #!/usr/bin/env python
 #this code is WTFPL, please reuse it, improve it and share it as much as you like and pay me a beer !
 #(c)2011 guyzmo at leloop dot org
+#
+# NOT TESTED ON THE WILD !!!
 
 import sys, serial, fcntl, os
 
+
+def send_to_serial(serial, baudrate, infile):
+    fd = sys.stdin.fileno()
+    fl = fcntl.fcntl(fd, fcntl.F_GETFL)
+    fcntl.fcntl(fd, fcntl.F_SETFL, fl | os.O_NONBLOCK)
+
+    with serial.Serial(serial, baudrate) as ser:
+        print ser.readline()
+
+        # set binary mode
+        ser.write(bytearray([27,'0']))
+        print ser.readline()
+        ser.flush()
+
+    for pixel in matrix_adapt(infile):
+        ser.write(pixel)
+        print ser.read()
+        ser.flush()
 
 def clumper(s, count=4):
     """http://stackoverflow.com/questions/5676302/taking-four-items-instead-of-on-in-a-for-loop"""
     for x in range(0, len(s), count):
         yield s[x:x+count]
 
-fd = sys.stdin.fileno()
-fl = fcntl.fcntl(fd, fcntl.F_GETFL)
-fcntl.fcntl(fd, fcntl.F_SETFL, fl | os.O_NONBLOCK)
-
-if len(sys.argv) != 4:
-    print "Usage: bitmap2screen /dev/ttyUSB0 9600 file.bitmap"
-    exit(1)
-
-with serial.Serial(sys.argv[1], baudrate = sys.argv[2]) as ser:
-    print ser.readline()
-
-    ser.write(bytearray([27,'0']))
-    #ser.write('\x1b')
-    #ser.write('0')
-    print ser.readline()
-
-    with open(sys.argv[3],'r') as infile:
-        # TODO : transform the bitmap file so it is read the right way on screen
-
+def matrix_adapt(file):
+    with open(file,'r') as infile:
         lines = infile.readlines()
+        dotmat = []
 
-        # seperate all the displays, by iteratings 8 lines by 8 lines
-        for disp in clumper(lines,8):
-            # for each group of lines, get all the columns by zipping them
-            for col in zip(*disp):
-                # for each column, print the pixels
-                for pixel in col:
-                    ser.write(pixel)
+        for disp in clumper(lines,8*96):
+            disp.reverse()
+            for cols in clumper(disp,8):
+                cols.reverse()
+                for col in zip(*cols):
+                    for pixel in col:
+                        if pixel == '\n':
+                            continue
+                        if pixel == ' ' or pixel == '0':
+                            dotmat += [0]
+                        else:
+                            dotmat += [1]
+        dotmat.reverse()
+        return dotmat
 
-        #for col in zip(*lines):
-        #    for pixels in clumper(col,8):
-        #        for pixel in pixels:
-        #            ser.write(pixels[0])
+def write_to_screen(serial,pixels):
+    for pix in matrix_adapt(sys.argv[1]):
+        print pix,
 
-        #for line in lines:
-        #    for c in line:
-        #        ser.write(c)
-    ser.flush()
 
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        print "Usage: bitmap2screen /dev/ttyUSB0 9600 file.bitmap"
+        exit(1)
+
+    init_serial(sys.argv[1],sys.argv[2],sys.argv[3])
